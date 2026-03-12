d1d8c8e028397ebd1fcc6dedc794182bdc29f7fb
Abhay <abhaysachdeva212@gmail.com>
2026-03-12 14:00:00 +0530

    feat: setup client-side routing and PrivateRoute

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const Spinner = () => (
+  <Fragment>
+    <div className="spinner-container">
+      <div className="spinner"></div>
+    </div>
+  </Fragment>
+);
+
+export default Spinner;
+

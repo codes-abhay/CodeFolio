9a8fc377c2171fc0af6ed7f98502355940880c87
Abhay <abhaysachdeva212@gmail.com>
2026-03-25 15:45:00 +0530

    feat: build developer profiles directory search view

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,62 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import ProfileItem from './ProfileItem';
+import { getProfiles } from '../../actions/profile';
+
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, [getProfiles]);
+
+  return (
+    <section className="container">
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <h1 className="large text-primary">Developers</h1>
+          <p className="lead">
+            <i className="fab fa-connectdevelop" /> Browse and connect with
+            developers
+          </p>
+          <div className="search-box mys-2">
+            <input
+              type="text"
+              placeholder="Search by name or skill..."
+              onChange={(e) => getProfiles(e.target.value)}
+              className="form-control"
+              style={{
+                width: '100%',
+                padding: '10px',
+                marginBottom: '20px',
+                fontSize: '16px'
+              }}
+            />
+          </div>
+          <div className="profiles">
+            {profiles.length > 0 ? (
+              profiles.map((profile) => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4>No profiles found...</h4>
+            )}
+          </div>
+        </Fragment>
+      )}
+    </section>
+  );
+};
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
